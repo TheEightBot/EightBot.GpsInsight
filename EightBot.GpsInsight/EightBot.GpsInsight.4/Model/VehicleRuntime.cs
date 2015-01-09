43cc21a6bb3118804dbfc0bedfb674f2caea66d7
nathanyorke <nathanyorke@b4708c22-8bd7-4d1a-8bcc-1959c71ce1a7>
2015-01-09 21:32:49 +0000

    new runtime_lifetime property

---
+++ EightBot.GpsInsight/EightBot.GpsInsight.4/Model/VehicleRuntime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightBot.GpsInsight.Model
+{
+    public class VehicleRuntime
+    {
+        public string id { get; set; }
+        public string vin { get; set; }
+        public string label { get; set; }
+        public long serial_number { get; set; }
+        public int age_minutes { get; set; }
+        public string fix_time { get; set; }
+        public double odometer { get; set; }
+        public string runtime_hours { get; set; }
+        public string runtime_lifetime { get; set; }
+        public string runtime_desc { get; set; }
+    }
+}
